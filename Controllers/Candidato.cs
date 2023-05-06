2868b0337a9dc25561944a9b892e19517ae9c895
Marcela Estrada <57819286+marcelaestrada@users.noreply.github.com>
2023-05-05 20:51:19 -0600

    First

---
+++ Controllers/Candidato.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using FinalSoftware.Models;
+
+namespace FinalSoftware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Candidato : ControllerBase
+    {
+        const string connectionString = "mongodb+srv://root:hola@finalsoftware.gqfaijh.mongodb.net/?retryWrites=true&w=majority";
+        static MongoClient mongoClient = new MongoClient(connectionString);
+        static IMongoDatabase database = mongoClient.GetDatabase("FinalSoftware");
+        IMongoCollection<CandidatoInfo> candidatos = database.GetCollection<CandidatoInfo>("Candidatos");
+
+        [HttpPost("crear")]
+        public async Task<ActionResult<Candidato>> CrearCandidato(CandidatoInfo candidato)
+        {
+            var candidatoNuevo = new BsonDocument {
+                {"DPI", candidato.DPI},
+                {"Nombre", candidato.Nombre},
+                {"Edad", candidato.Edad},
+                {"Profesion", candidato.Profesion},
+                {"Cargo", candidato.Cargo},
+                {"Descripcion",candidato.Descripcion}
+            };
+
+            candidatos.InsertOne(candidato);
+            return CreatedAtAction(nameof(CrearCandidato), new { id = candidato.DPI }, candidato);
+        }
+    }
+}
---
+++ Models/CandidatoInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalSoftware.Models
+{
+    public class CandidatoInfo
+    {
+        public string DPI { get; set; }
+        public string Nombre { get; set; }
+        public string Edad { get; set; }
+        public string Profesion { get; set; }
+        public string Cargo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
