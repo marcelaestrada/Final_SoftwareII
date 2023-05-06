99292c70de1ffc3336305c065d5400f835230987
Marcela Estrada <57819286+marcelaestrada@users.noreply.github.com>
2023-05-05 20:55:54 -0600

    VotanteInfo

--- Models/VotanteInfo.cs
+++ Models/VotanteInfo.cs
@@ -7,5 +7,11 @@ namespace FinalSoftware.Models
 {
     public class VotanteInfo
     {
+        public string DPI { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Hora { get; set; }
+        public DateTime Fecha { get; set; }
+        public string IP { get; set; }
     }
 }
