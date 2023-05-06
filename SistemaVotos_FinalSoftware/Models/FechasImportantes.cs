cc3388c1584b1f2a999e905d0b78ac79f7cc2bc5
Marcela Estrada <57819286+marcelaestrada@users.noreply.github.com>
2023-05-05 21:29:19 -0600

    Fechas

---
+++ SistemaVotos_FinalSoftware/Models/FechasImportantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVotos_FinalSoftware.Models
+{
+    public class FechasImportantes
+    {
+        public DateTime inicioCandidatos = Convert.ToDateTime("05-31-2023");
+        public DateTime finCandidatos = Convert.ToDateTime("06-30-2023");
+        public DateTime inicioVotacioines = Convert.ToDateTime("07-15-2023");
+        public DateTime finVotaciones = Convert.ToDateTime("07-20-2023");
+    }
+}
